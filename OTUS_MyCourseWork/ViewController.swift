e354424e8af9e7176a8fc2308ebc4f356def0ce6
B0mbastic <kovbasin@gmai.com>
2023-02-28 01:05:10 +0800

    Initial Commit

---
+++ OTUS_MyCourseWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTUS_MyCourseWork
+//
+//  Created by Александр Ковбасин on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
