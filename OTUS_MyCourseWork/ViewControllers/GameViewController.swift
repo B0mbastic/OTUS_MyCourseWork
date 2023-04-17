df5f2d9b53d8ce33c588b47929d1b2d4dd261838
B0mbastic <kovbasin@gmai.com>
2023-04-17 15:12:17 +0800

    Added all buttons to toy, working logic without shorter subsequence

--- OTUS_MyCourseWork/ViewControllers/GameViewController.swift
+++ OTUS_MyCourseWork/ViewControllers/GameViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 import AVFoundation
 
 class GameViewController: UIViewController {
-    private lazy var sizeConstant: Int = Int(view.frame.width * 0.7)
+    private lazy var sizeConstant: CGFloat = view.frame.width * 0.7
     
     private lazy var mainLabel: UILabel = {
         let label = UILabel()
@@ -25,59 +25,99 @@ class GameViewController: UIViewController {
         image.layer.zPosition = 2
         return image
     }()
-    private lazy var YellowLight: UIImageView = {
+    private lazy var yellowLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "yellowOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var GreenLight: UIImageView = {
+    private lazy var greenLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "greenOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var VioletLight: UIImageView = {
+    private lazy var violetLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "violetOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var BlueLight: UIImageView = {
+    private lazy var blueLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "blueOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var OrangeLight: UIImageView = {
+    private lazy var orangeLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "orangeOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var RedLight: UIImageView = {
+    private lazy var redLight: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "redOn.png")
         image.layer.zPosition = 1
         return image
     }()
-    private lazy var yellowButton: UIButton = {
+    private lazy var violetButton: UIButton = {
         let button = UIButton()
-        button.tag = 1
-        //button.backgroundColor = .yellow
-        button.layer.cornerRadius = 50
+        button.tag = 0
+        //button.backgroundColor = .systemPurple
+        button.layer.cornerRadius = sizeConstant/6
         button.layer.zPosition = 5
-        button.isHidden = false
+        button.isHidden = true
         button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
         return button
     }()
     private lazy var redButton: UIButton = {
         let button = UIButton()
-        button.tag = 0
-        //button.backgroundColor = .red
-        button.layer.cornerRadius = 50
+        button.tag = 1
+        //button.backgroundColor = .systemRed
+        button.layer.cornerRadius = sizeConstant/6
         button.layer.zPosition = 5
-        button.isHidden = false
+        button.isHidden = true
+        button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        return button
+    }()
+    private lazy var yellowButton: UIButton = {
+        let button = UIButton()
+        button.tag = 2
+        //button.backgroundColor = .systemYellow
+        button.layer.cornerRadius = sizeConstant/6
+        button.layer.zPosition = 5
+        button.isHidden = true
+        button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        return button
+    }()
+    private lazy var greenButton: UIButton = {
+        let button = UIButton()
+        button.tag = 3
+        //button.backgroundColor = .systemGreen
+        button.layer.cornerRadius = sizeConstant/6
+        button.layer.zPosition = 5
+        button.isHidden = true
+        button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        return button
+    }()
+    private lazy var orangeButton: UIButton = {
+        let button = UIButton()
+        button.tag = 4
+        //button.backgroundColor = .systemOrange
+        button.layer.cornerRadius = sizeConstant/6
+        button.layer.zPosition = 5
+        button.isHidden = true
+        button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        return button
+    }()
+    private lazy var blueButton: UIButton = {
+        let button = UIButton()
+        button.tag = 5
+        //button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = sizeConstant/6
+        button.layer.zPosition = 5
+        button.isHidden = true
         button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
         return button
     }()
@@ -94,16 +134,14 @@ class GameViewController: UIViewController {
         return button
     }()
     
-    //var lightQueue: [UIImageView] = []
-
-//    private lazy var lightQueue = [LampModel(id: 0, view: RedLight), LampModel(id: 1, view: YellowLight), LampModel(id: 2, view: GreenLight), LampModel(id: 3, view: OrangeLight), LampModel(id: 4, view: BlueLight), LampModel(id: 5, view: VioletLight)].shuffled()
-    private lazy var lightQueue = [LampModel(id: 0, view: RedLight), LampModel(id: 1, view: YellowLight)].shuffled()
+    private lazy var lightQueue = [LampModel(id: 0, view: violetLight), LampModel(id: 1, view: redLight), LampModel(id: 2, view: yellowLight), LampModel(id: 3, view: greenLight), LampModel(id: 4, view: orangeLight), LampModel(id: 5, view: blueLight)].shuffled()
+    
     
     let yellowSound = URL(fileURLWithPath: Bundle.main.path(forResource: "yellowSound", ofType: "wav")!)
     var audioPlayer = AVAudioPlayer()
     let blinkSpeed: Double = 0.5
     //var gameSize = 300
-
+    
     var queue = OperationQueue()
     var gameSubsequence: [Int] = []
     var playerSubsequence: [Int] = []
@@ -111,14 +149,18 @@ class GameViewController: UIViewController {
     
     private func setupViews() {
         view.addSubview(toyImageView)
-        view.addSubview(YellowLight)
-        view.addSubview(GreenLight)
-        view.addSubview(VioletLight)
-        view.addSubview(BlueLight)
-        view.addSubview(OrangeLight)
-        view.addSubview(RedLight)
-        view.addSubview(yellowButton)
+        view.addSubview(violetLight)
+        view.addSubview(redLight)
+        view.addSubview(yellowLight)
+        view.addSubview(greenLight)
+        view.addSubview(orangeLight)
+        view.addSubview(blueLight)
+        view.addSubview(violetButton)
         view.addSubview(redButton)
+        view.addSubview(yellowButton)
+        view.addSubview(greenButton)
+        view.addSubview(orangeButton)
+        view.addSubview(blueButton)
         view.addSubview(startButton)
         view.addSubview(mainLabel)
         
@@ -126,51 +168,70 @@ class GameViewController: UIViewController {
             make.centerX.equalToSuperview()
             make.bottom.equalTo(toyImageView.snp.top).offset(-40)
         }
-        
         toyImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        GreenLight.snp.makeConstraints { make in
+        violetLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        VioletLight.snp.makeConstraints { make in
+        redLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        YellowLight.snp.makeConstraints { make in
+        yellowLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        BlueLight.snp.makeConstraints { make in
+        greenLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        OrangeLight.snp.makeConstraints { make in
+        orangeLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        RedLight.snp.makeConstraints { make in
+        blueLight.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
             make.width.height.equalTo(sizeConstant)
         }
-        yellowButton.snp.makeConstraints { make in
-            make.centerX.equalTo(toyImageView.snp.centerX).offset(90)
-            make.centerY.equalTo(toyImageView.snp.centerY).offset(50)
-            make.width.height.equalTo(100)
+        violetButton.snp.makeConstraints { make in
+            make.centerX.equalTo(toyImageView.snp.centerX)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(-sizeConstant/3)
+            make.width.height.equalTo(sizeConstant/3)
         }
         redButton.snp.makeConstraints { make in
-            make.centerX.equalTo(toyImageView.snp.centerX).offset(90)
-            make.centerY.equalTo(toyImageView.snp.centerY).offset(-50)
-            make.width.height.equalTo(100)
+            make.centerX.equalTo(toyImageView.snp.centerX).offset(sizeConstant*0.3)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(-sizeConstant/6)
+            make.width.height.equalTo(sizeConstant/3)
+        }
+        yellowButton.snp.makeConstraints { make in
+            make.centerX.equalTo(toyImageView.snp.centerX).offset(sizeConstant*0.3)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(sizeConstant/6)
+            make.width.height.equalTo(sizeConstant/3)
+        }
+        greenButton.snp.makeConstraints { make in
+            make.centerX.equalTo(toyImageView.snp.centerX)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(sizeConstant/3)
+            make.width.height.equalTo(sizeConstant/3)
+        }
+        orangeButton.snp.makeConstraints { make in
+            make.centerX.equalTo(toyImageView.snp.centerX).offset(-sizeConstant*0.3)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(sizeConstant/6)
+            make.width.height.equalTo(sizeConstant/3)
+        }
+        blueButton.snp.makeConstraints { make in
+            make.centerX.equalTo(toyImageView.snp.centerX).offset(-sizeConstant*0.3)
+            make.centerY.equalTo(toyImageView.snp.centerY).offset(-sizeConstant/6)
+            make.width.height.equalTo(sizeConstant/3)
         }
         startButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -184,118 +245,46 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupViews()
-        
-        
-        //        for (index, value) in lightQueue.enumerated() {
-        //                        DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) + blinkSpeed) {
-        //                            value.layer.zPosition = 3
-        //                        }
-        //
-        //                        DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) + blinkSpeed*2) {
-        //                            value.layer.zPosition = 1
-        //                            self.RedButton.isHidden = false
-        //                        }
-        //                }
-        
         queue.maxConcurrentOperationCount = 1
-//        lightQueue = [VioletLight, RedLight, YellowLight, GreenLight, OrangeLight, BlueLight]
-//        lightQueue = lightQueue.shuffled()
-//
-        gameSubsequence = [0, 1, 2, 3, 4, 5]
-        
-
-
-        
-        
-        
-        
-        
-        //        queue.addOperation { [weak self] in
-        //
-        //            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-        //                self?.RedButton.isHidden = false
-        //                self?.YellowButton.isHidden = false
-        //            }
-        //        }
-        
-        
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-        //            self.YellowLight.layer.zPosition = 3
-        //        }
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-        //            self.YellowLight.layer.zPosition = 1
-        //        }
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-        //            self.GreenLight.layer.zPosition = 3
-        //        }
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-        //            self.GreenLight.layer.zPosition = 1
-        //        }
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        //            self.VioletLight.layer.zPosition = 3
-        //        }
-        //        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
-        //            self.VioletLight.layer.zPosition = 1
-        //        }
-        
     }
     
-    //    override func viewDidAppear(_ animated: Bool) {
-    //        super.viewDidAppear(animated)
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //        //YellowLight.layer.zPosition = 1
-    //
-    //
-    //    }
-    //
     func lightLamp(lampImageView: UIImageView) {
-        //            do {
-        //                 audioPlayer = try AVAudioPlayer(contentsOf: yellowSound)
-        //                 audioPlayer.play()
-        //            } catch {
-        //               // couldn't load file :(
-        //            }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: yellowSound)
+            audioPlayer.play()
+        } catch {
+            // couldn't load file :(
+        }
         
         lampImageView.layer.zPosition = 3
-        
-        //lampImageView.layer.zPosition = 3
         DispatchQueue.main.asyncAfter(deadline: .now() + blinkSpeed) {
             lampImageView.layer.zPosition = 1
         }
     }
     
     
-//    @objc func startGame(sender: UIButton!){
-//        playerSubsequence = []
-//        redButton.isHidden = false
-//        yellowButton.isHidden = false
-//        mainLabel.isHidden = true
-//        startButton.isHidden = true
-//    }
+    //    @objc func startGame(sender: UIButton!){
+    //        playerSubsequence = []
+    //        redButton.isHidden = false
+    //        yellowButton.isHidden = false
+    //        mainLabel.isHidden = true
+    //        startButton.isHidden = true
+    //    }
     
     @objc func startGame(sender: UIButton!){
         enum NetworkError: Error {
             case customError
         }
-        playerSubsequence = []
-        redButton.isHidden = false
-        yellowButton.isHidden = false
         mainLabel.isHidden = true
         startButton.isHidden = true
-        startButton.isHidden = true
+        playerSubsequence = []
         
         for item in lightQueue {
             queue.addOperation { [weak self] in
                 DispatchQueue.main.async {
                     self?.lightLamp(lampImageView: item.view)
                 }
-
+                
                 self?.queue.isSuspended = true
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
                     self?.queue.isSuspended = false
@@ -304,7 +293,7 @@ class GameViewController: UIViewController {
         }
         queue.addOperation {
             DispatchQueue.main.async {
-                self.startButton.isHidden = false
+                self.showButtons()
             }
         }
     }
@@ -313,27 +302,25 @@ class GameViewController: UIViewController {
         let tag: Int = sender.tag
         let lamp: UIImageView
         switch tag {
-        case 0: lamp = RedLight
-        case 1: lamp = YellowLight
+        case 0: lamp = violetLight
+        case 1: lamp = redLight
+        case 2: lamp = yellowLight
+        case 3: lamp = greenLight
+        case 4: lamp = orangeLight
+        case 5: lamp = blueLight
         default: return
         }
         lightLamp(lampImageView: lamp)
         playerSubsequence.append(sender.tag)
-            if isEqualArray(playerSubsequence, with: lightQueue) {
-                if playerSubsequence.count == lightQueue.count {
-                    mainLabel.text = "YOU WIN!"
-                    redButton.isHidden = true
-                    yellowButton.isHidden = true
-                    mainLabel.isHidden = false
-                    startButton.isHidden = false
-                }
-            } else {
-                mainLabel.text = "YOU LOOSE!"
-                redButton.isHidden = true
-                yellowButton.isHidden = true
-                mainLabel.isHidden = false
-                startButton.isHidden = false
+        if isEqualArray(playerSubsequence, with: lightQueue) {
+            if playerSubsequence.count == lightQueue.count {
+                mainLabel.text = "YOU WIN!"
+                hideButtons()
             }
+        } else {
+            hideButtons()
+            mainLabel.text = "YOU LOOSE!"
+        }
     }
     func isEqualArray(_ array1: [Int], with array2: [LampModel]) -> Bool {
         var isEqual: Bool = true
@@ -345,6 +332,26 @@ class GameViewController: UIViewController {
         }
         return isEqual
         //return Set(array1).intersection(array2).count == 0 ? false : true
+    }
+    func hideButtons() {
+        redButton.isHidden = true
+        yellowButton.isHidden = true
+        greenButton.isHidden = true
+        orangeButton.isHidden = true
+        blueButton.isHidden = true
+        violetButton.isHidden = true
+        mainLabel.isHidden = false
+        startButton.isHidden = false
+    }
+    func showButtons() {
+        redButton.isHidden = false
+        yellowButton.isHidden = false
+        greenButton.isHidden = false
+        orangeButton.isHidden = false
+        blueButton.isHidden = false
+        violetButton.isHidden = false
+        mainLabel.isHidden = true
+        startButton.isHidden = true
     }
 }
 
